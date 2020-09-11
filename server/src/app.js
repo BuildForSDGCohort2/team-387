3bc6a4613d6c0d02caf80b78abfc1fec069ed314
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-09-11 01:17:15 +0100

    Fixes #3 Added Swaggee docs

--- server/src/app.js
+++ server/src/app.js
@@ -4,16 +4,21 @@ import 'regenerator-runtime/runtime';
 import express, { json, urlencoded } from 'express';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import cors from 'cors';
+import SwaggerUI from 'swagger-ui-express';
 
+import swaggerSpec from './utils/swagger';
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 const app = express();
 
+app.use(cors);
 app.use(logger('dev'));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use('/api-docs', SwaggerUI.serve, SwaggerUI.setup(swaggerSpec));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
