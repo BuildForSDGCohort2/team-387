aebc1660e8368c58299572e5ab024a983b448019
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-09-12 01:17:14 +0100

    2nd update for routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -4,7 +4,7 @@ import { Router } from 'express';
 const router = Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+router.get('/users', (req, res, next) => {
   res.send('respond with a resource');
 });
 
