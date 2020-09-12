cb25bff8c91f7337beffd7d9d4e084807ae73cb0
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2020-09-12 01:14:05 +0100

    Upadtd eslint for routes

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,9 +1,10 @@
+/* eslint-disable no-unused-vars */
 import { Router } from 'express';
 
 const router = Router();
 
 /* GET users listing. */
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
